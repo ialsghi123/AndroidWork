8d4a38ba1319992b3086206c9a8bd928bb7ee888
kit-lec <kit-lec@hotmail.com>
2020-04-14 11:05:03 +0900

    학원커밋3-1

--- a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
+++ a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.linear4);
 
         int ss = 100;
-        String tt = "하하";
+        String tt = "호호호호";
 
     }
 }
