cbf49f61c5d890618db4c513eab1b6129aabeab3
ialsghi123 <ialsghi123@naver.com>
2020-04-15 20:21:35 +0900

    계산기

--- a003_listener/src/main/java/com/lec/android/a003_widget/Main2Activity.java
+++ a003_listener/src/main/java/com/lec/android/a003_widget/Main2Activity.java
@@ -20,6 +20,7 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 //
+        //
         apply();
 
 
